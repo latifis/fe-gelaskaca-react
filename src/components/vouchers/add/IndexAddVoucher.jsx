b493e8d81464fd3bdca97ee14dd7fe880ca486b8
Latief Irfansyah <latifirfansyah@gmail.com>
2022-12-06 04:52:55 +0700

    add vouchers

---
+++ src/components/vouchers/add/IndexAddVoucher.jsx
@@ -0,0 +1,77 @@
+import Navbar from "../../Navbar";
+import Footer from "../../Footer";
+import VoucherSidebar from "../VoucherSidebar";
+import VoucherNavbar from "../VoucherNavbar";
+import { Link } from "react-router-dom";
+
+function IndexAddVoucher() {
+    return (
+        <div>
+            <div className='my-8 mx-12'>
+                <Navbar />
+                <div>
+                    <section className="bg-gray-100 py-3 px-11 my-5">
+                        <div className="container">
+                            <ul className="breadcrumb">
+                                <li>
+                                    <Link to="/voucher">Vouchers</Link>
+                                </li>
+                                <li>
+                                    <Link to="/voucher/add-voucher" aria-label="current-page">
+                                        Add Voucher
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+                <section className="flex py-4 my-8">
+                    <VoucherSidebar />
+                </section>
+            </div>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    )
+}
+
+const items = [
+    {
+        name: "Latif",
+        email: "latifadmin@gmail.com",
+        age: 22,
+        role: "ADMIN",
+        phone: "0822223432234",
+    },
+    {
+        name: "Latifpic",
+        email: "latifpicn@gmail.com",
+        age: 21,
+        role: "PIC",
+        phone: "08222222368",
+    },
+    {
+        name: "Latifpic",
+        email: "latifpicn@gmail.com",
+        age: 21,
+        role: "PIC",
+        phone: "08222222368",
+    },
+    {
+        name: "Latifpic",
+        email: "latifpicn@gmail.com",
+        age: 21,
+        role: "PIC",
+        phone: "08222222368",
+    },
+    {
+        name: "Latifpic",
+        email: "latifpicn@gmail.com",
+        age: 21,
+        role: "PIC",
+        phone: "08222222368",
+    }
+]
+
+export default IndexAddVoucher;
